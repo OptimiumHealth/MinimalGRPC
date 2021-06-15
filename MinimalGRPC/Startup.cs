38525ce6fc2ab89467745fd6c3865b086eba03e4
Mark Stega <mark2002@stega.us>
2021-06-15 10:25:57 -0400

    2021-06-15 net5/plaintext

--- MinimalGRPC/Startup.cs
+++ MinimalGRPC/Startup.cs
@@ -1,5 +1,27 @@
 #define xSSL
+#define xnet31
 
+#if net31
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using MinimalGRPC.gRPCClient;
+
+using Optimiser.Web.Services;
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+#else
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 
@@ -13,8 +35,8 @@ using MinimalGRPC.gRPCClient;
 
 using Optimiser.Web.Services;
 
-using System;
 using System.Net.Http;
+#endif
 
 namespace MinimalGRPC
 {
@@ -38,6 +60,12 @@ namespace MinimalGRPC
             var baseUri = "https://localhost:44324";
 #else
             var baseUri = "http://localhost:45958";
+
+#if net31
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            AppContext.SetSwitch(
+                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+#endif
 #endif
 
             // Create a gRPC-Web channel pointing to the backend server
@@ -58,6 +86,7 @@ namespace MinimalGRPC
             });
 
             services.AddGrpc();
+            services.AddGrpcReflection();
 
         }
 
@@ -83,12 +112,75 @@ namespace MinimalGRPC
 
             app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
+#if net31
+            app.Use((c, next) =>
+            {
+                if (c.Request.ContentType == "application/grpc")
+                {
+                    var current = c.Features.Get<IHttpResponseFeature>();
+                    c.Features.Set<IHttpResponseFeature>(new HttpSysWorkaroundHttpResponseFeature(current));
+                }
+                return next();
+            });
+#endif
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<WeatherService>();
+                endpoints.MapGrpcReflectionService();
                 endpoints.MapBlazorHub();
                 endpoints.MapFallbackToPage("/_Host");
             });
         }
     }
+
+#if net31
+    public class HttpSysWorkaroundHttpResponseFeature : IHttpResponseFeature
+    {
+        private readonly IHttpResponseFeature _inner;
+
+        public HttpSysWorkaroundHttpResponseFeature(IHttpResponseFeature inner)
+        {
+            _inner = inner;
+            _inner.OnStarting(o =>
+            {
+                HasStarted = true;
+                return Task.CompletedTask;
+            }, null);
+        }
+
+        [Obsolete]
+        public Stream Body
+        {
+            get => _inner.Body;
+            set { _inner.Body = value; }
+        }
+        public bool HasStarted { get; private set; }
+        public IHeaderDictionary Headers
+        {
+            get => _inner.Headers;
+            set { _inner.Headers = value; }
+        }
+        public string ReasonPhrase
+        {
+            get => _inner.ReasonPhrase;
+            set { _inner.ReasonPhrase = value; }
+        }
+        public int StatusCode
+        {
+            get => _inner.StatusCode;
+            set { _inner.StatusCode = value; }
+        }
+
+        public void OnCompleted(Func<object, Task> callback, object state)
+        {
+            _inner.OnCompleted(callback, state);
+        }
+
+        public void OnStarting(Func<object, Task> callback, object state)
+        {
+            _inner.OnStarting(callback, state);
+        }
+    }
+#endif
 }
